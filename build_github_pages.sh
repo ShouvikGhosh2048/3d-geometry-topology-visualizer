66ad47a225c58fe436516c1922a64732593414e3
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2025-01-22 22:11:59 +0530

    Update action

--- build_github_pages.sh
+++ build_github_pages.sh
@@ -10,7 +10,7 @@ git clone https://github.com/emscripten-core/emsdk.git
 ./emsdk/emsdk activate latest
 
 EMSCRIPTEN_SDK_DIR="emsdk"
-OUT_DIR="game_web"
+OUT_DIR="build/web"
 
 mkdir -p $OUT_DIR
 
@@ -18,17 +18,13 @@ export EMSDK_QUIET=1
 # shellcheck disable=SC1091
 [[ -f "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh" ]] && . "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh"
 
-if ! odin-linux-amd64-nightly+2025-01-08/odin build main_web -target:freestanding_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -vet -strict-style -o:speed -out:$OUT_DIR/game; then
-  exit 1
-fi
+odin-linux-amd64-nightly+2025-01-08/odin build source/main_web -target:freestanding_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -vet -strict-style -o:speed -out:$OUT_DIR/game
 
-ODIN_PATH=$(odin-linux-amd64-nightly+2025-01-08/odin root)
-files="main_web/main_web.c $OUT_DIR/game.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a"
-flags="-sUSE_GLFW=3 -sASYNCIFY -sASSERTIONS -DPLATFORM_WEB"
-custom="--shell-file main_web/index_template.html"
+files="main_web/main_web.c $OUT_DIR/game.wasm.o odin-linux-amd64-nightly+2025-01-08/vendor/raylib/wasm/libraylib.a"
+flags="-sUSE_GLFW=3 -sASSERTIONS --shell-file source/main_web/index_template.html --preload-file assets"
 
 # shellcheck disable=SC2086
 # Add `-g` to `emcc` call to enable debug symbols (works in chrome).
-emcc -g -o $OUT_DIR/index.html $files $flags $custom && rm $OUT_DIR/game.wasm.o
+emcc -o $OUT_DIR/index.html $files $flags && rm $OUT_DIR/game.wasm.o
 
 echo "Web build created in ${OUT_DIR}"
